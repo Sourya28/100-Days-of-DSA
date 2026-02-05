fb8885e1df25a1a08b00c515c4853e7e88e116f3
Sourya Pokhariya <pokhariyasourya28@gmail.com>
2026-02-05 20:39:49 +0530

    Add files via upload

---
+++ day_5.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int p, q;
+
+    scanf("%d", &p);
+    int *log1 = (int*)malloc(p * sizeof(int));
+    for (int i = 0; i < p; i++) 
+    {
+        scanf("%d", &log1[i]);
+    }
+
+    scanf("%d", &q);
+    int *log2 = (int*)malloc(q * sizeof(int));
+    for (int i = 0; i < q; i++) 
+    {
+        scanf("%d", &log2[i]);
+    }
+
+    int i = 0, j = 0;
+
+    while (i < p && j < q) 
+    {
+        if (log1[i] < log2[j]) 
+        {
+            printf("%d ", log1[i]);
+            i++;
+        } else 
+        {
+            printf("%d ", log2[j]);
+            j++;
+        }
+    }
+
+    while (i < p) 
+    {
+        printf("%d ", log1[i]);
+        i++;
+    }
+    while (j < q) 
+    {
+        printf("%d ", log2[j]);
+        j++;
+    }
+
+    printf("\n");
+
+    free(log1);
+    free(log2);
+
+    return 0;
+}
