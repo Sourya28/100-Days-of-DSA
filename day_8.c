1467fb19c4eddd454f94a1d46a74e6eb9c084233
Sourya Pokhariya <pokhariyasourya28@gmail.com>
2026-02-08 21:15:34 +0530

    Add files via upload

---
+++ day_8.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+long long power(int a, int b) 
+{
+    if (b == 0) 
+    {
+        return 1;
+    }
+    return a * power(a, b - 1);
+}
+
+int main() 
+{
+    int a, b;
+    
+    if (scanf("%d %d", &a, &b) == 2) 
+    {
+        if (b < 0) 
+        {
+            printf("This simple recursion handles non-negative exponents.\n");
+        } 
+        else 
+        {
+            printf("%lld\n", power(a, b));
+        }
+    }
+    
+    return 0;
+}
